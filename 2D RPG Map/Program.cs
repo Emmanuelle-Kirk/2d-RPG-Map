8e010a218c0fd2f83c5528dd7c84faa010dbe8ec
Emmanuelle-Kirk <wolfeetried@gmail.com>
2021-11-03 14:31:46 -0300

    Update Program.cs

--- 2D RPG Map/Program.cs
+++ 2D RPG Map/Program.cs
@@ -4,16 +4,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2D_RPG_Map
+namespace GameLoop
 {
     class Program
     {
+        static bool gameOver = (false);
+        static int x;
+        static int y;
+        //if input is??????
 
+        static void PlayerDraw()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.Write(".");
+        }
+        static void Exit()
+        {
+            Console.SetCursorPosition(116, 28);
+            Console.Write("O");
+        }
+        static void PlayerUpdate()
+        {
+            ConsoleKeyInfo readKeyInput = Console.ReadKey(true); //ReadKey stored
+            char input = readKeyInput.KeyChar; //extracting char
+            if (input == 'w')
+            {
+                y = y - 1;
+                //Console.WriteLine("y = " + y);
+            }
+            if (input == 'a')
+            {
+                x = x - 1;
+                //Console.WriteLine("x = " + x);
+            }
+            if (input == 's')
+            {
+                y = y + 1;
+                //Console.WriteLine("y = " + y);
+            }
+            if (input == 'd')
+            {
+                x = x + 1;
+                //Console.WriteLine("X = " + x);
+            }
+            if (x <= -1)
+            {
+                x = x + 1;
+            }
+            if (y <= -1)
+            {
+                y = y + 1;
+            }
+            if (x >= 117)
+            {
+                x = x - 1;
+            }
+           if (y >= 29)
+            {
+                y = y - 1;
+            }
+           if (x == 116 && y == 28)
+            {
+                gameOver = true;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("2D RPG Map");
-
+            while (gameOver == false)
+            {
+                Exit();
+                PlayerUpdate();
+                PlayerDraw();
+            }
+            Console.WriteLine();
+            Console.WriteLine("GAME OVER");
             Console.ReadKey(true);
         }
     }
 }
+    
+
+    
+
